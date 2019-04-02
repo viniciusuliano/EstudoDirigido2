e45def65cf2ecf0b5e0367a74e9973adf13f7b69
Vinicius Uliano <48574234+viniciusuliano@users.noreply.github.com>
2019-04-02 08:50:53 -0300

    Create Exercicio12.php

---
+++ Exercicio12.php
@@ -0,0 +1,15 @@
+<?php
+$num=0;
+$contagem=1;
+print "Começando em ...3";
+       sleep(1) ;
+print  " ...2";
+       sleep(1);
+print  " ...1";
+       sleep(1);
+print  " ...0 ";
+       sleep(1);
+while ($contagem<=100)
+    {$num++;   
+     print "$num ";        
+     $contagem++;}
