31ab2fe19faee61be638fa140aaf709e801005f4
Vinicius Uliano <48574234+viniciusuliano@users.noreply.github.com>
2019-04-09 10:44:23 -0300

    Update Exercicio8.php

--- Exercicio8.php
+++ Exercicio8.php
@@ -1,20 +1,5 @@
 <?php
 print "Digite o preço do 1º produto: ";
-$num1= (float) fgets(STDIN);
-print "Digite o preço do 2º produto: ";
-$num2= (float) fgets(STDIN);
-print "Digite o preço do 3º segundo: ";
-$num3= (float) fgets(STDIN);
-if ($num1<$num2 && $num1<$num3) 
-    {print "O preço do 1º produto é mais barato\n";}
-if ($num2<$num1 && $num2<$num3) 
-    {print "O preço do 2º produto é mais barato\n";}
-if ($num3<$num1 && $num3<$num2) 
-    {print "O preço do 3º produto é mais barato\n";}
-if ($num1==$num2 && $num2==$num3) 
-    {print "Todos os produtos custam a mesma quantia\n";}
-<?php
-print "Digite o preço do 1º produto: ";
 $numero1= (float) fgets(STDIN);
 print "Digite o preço do 2º produto: ";
 $numero2= (float) fgets(STDIN);
