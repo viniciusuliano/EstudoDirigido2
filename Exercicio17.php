87e4f4ede97a7aa5fc9b8a62a296fb5ecc9e5bc5
Vinicius Uliano <48574234+viniciusuliano@users.noreply.github.com>
2019-04-09 11:08:42 -0300

    Update Exercicio17.php

--- Exercicio17.php
+++ Exercicio17.php
@@ -1,11 +1,10 @@
 <?php
-
 print "Digite a base: ";
 $base= (float) fgets(STDIN);
 
 print "Digite o expoente: ";
-$expoente= (float) fgets(STDIN);
 
+$expoente= (float) fgets(STDIN);
 $potencia= ($base**$expoente);
 
 print "\nO resultado é: $potencia";
